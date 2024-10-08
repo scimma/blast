9a2854b2b1560f1187b9ac7ab5c33aa5a03989ca
David Jones <djones1741@gmail.com>
2024-10-08 09:14:45 -1000

    added comments for future to-do

--- app/host/system_tasks.py
+++ app/host/system_tasks.py
@@ -55,6 +55,8 @@ class TNSDataIngestion(SystemTaskRunner):
                     tasks = TaskRegister.objects.filter(
                         transient=saved_transient)
                     for t in tasks:
+                        ### this could be smarter
+                        ### we don't *always* need to re-process every stage
                         t.status = Status.objects.get(message="not processed")
                         t.save()
 
