6d21086b673da53249d6f08212bdafba1dea3a21
T. Andrew Manning <manninga@illinois.edu>
2025-02-28 09:20:24 -0600

    Hide login button on home page

--- app/host/views.py
+++ app/host/views.py
@@ -542,6 +542,7 @@ def update_home_page_statistics():
             "processed": processed,
             "in_progress": in_progress,
             **bokeh_processing_context,
+            "hide_login": True,
         },
     )
     with open(os.path.join(settings.STATIC_ROOT, 'index.html'), 'w') as fp:
