5314c66b5bce20b0fbc71cfde083bf38f9c12aeb
T. Andrew Manning <manninga@illinois.edu>
2025-03-25 20:54:17 -0500

    Retriggered workflows restart tasks with status "processing"

--- app/host/tasks.py
+++ app/host/tasks.py
@@ -5,7 +5,7 @@ from celery import shared_task
 from host.base_tasks import task_soft_time_limit
 from host.base_tasks import task_time_limit
 from host.workflow import transient_workflow
-from .models import Transient
+from .models import Transient, Status, TaskRegister
 from host.system_tasks import DeleteGHOSTFiles
 from host.system_tasks import IngestMissedTNSTransients
 from host.system_tasks import InitializeTransientTasks
@@ -49,6 +49,15 @@ def import_transient_list(transient_names, retrigger=False):
     for transient in existing_transients:
         if retrigger:
             print(f'Retriggering workflow for transient "{transient.name}"')
+            # Assume that any task in a "processing" state is not actually running,
+            # resetting the status to "not processed" so that it will run again.
+            # TODO: Search Celery queue and terminate any actively running tasks first.
+            processing_status = Status.objects.get(message__exact="processing")
+            not_processed_status = Status.objects.get(message__exact="not processed")
+            tasks = TaskRegister.objects.filter(transient=transient)
+            for task in [task for task in tasks if task.status == processing_status]:
+                task.status = not_processed_status
+                task.save()
             process_transient(transient.name)
         else:
             print(f'Skipping existing transient "{transient.name}"')
