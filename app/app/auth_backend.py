1715d870ba66b900d756102a9f4c7f41822f3a09
T. Andrew Manning <manninga@illinois.edu>
2025-06-25 16:20:56 -0500

    Make username creation algorithm more flexible against OIDC claims

--- app/app/auth_backend.py
+++ app/app/auth_backend.py
@@ -7,21 +7,35 @@ from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 from django.conf import settings
 from django.core.exceptions import ObjectDoesNotExist
 import unicodedata
+import re
 
 
 class KeycloakOIDCAuthenticationBackend(OIDCAuthenticationBackend):
     def __init__(self):
         super().__init__(self)
 
-    
     def create_user(self, claims):
         """ Overrides Authentication Backend so that Django users are
             created with the keycloak preferred_username.
             If nothing found matching the email, then try the username.
         """
+        ''' As of 2025/06/25, CILogon provides these claims:
+            {
+                'name': 'Jon Bon Jovi',
+                'given_name': 'Jon Bon',
+                'family_name': 'Jovi',
+                'email': 'rockstar@example.com'
+                'sub': 'http://cilogon.org/serverA/users/12345678',
+                'aud': 'cilogon:/client_id/71dd2716fc1f3460d49afbce1d74f068',
+                'acr': 'https://refeds.org/profile/mfa',
+                'azp': 'cilogon:/client_id/71dd2716fc1f3460d49afbce1d74f068',
+                'iss': 'https://cilogon.org',
+                'jti': 'https://cilogon.org/oauth2/idToken//ac2...389/17...62',
+            }
+        '''
         email = self.get_email(claims)
         user_info = {
-            'username':claims.get("preferred_username"),
+            'username': self.get_username(claims),
             'email': email,
             'first_name': claims.get('given_name', ''),
             'last_name': claims.get('family_name', ''),
@@ -33,7 +47,7 @@ class KeycloakOIDCAuthenticationBackend(OIDCAuthenticationBackend):
             last_name=user_info['last_name'],
         )
         return new_user
-    
+
     def filter_users_by_claims(self, claims):
         """ Return all users matching the specified email.
             If nothing found matching the email, then try the username
@@ -50,7 +64,7 @@ class KeycloakOIDCAuthenticationBackend(OIDCAuthenticationBackend):
                 return self.UserModel.objects.none()
             users = self.UserModel.objects.filter(username__iexact=preferred_username)
         return users
-    
+
     def update_user(self, user, claims):
         email = claims.get("email")
         user.first_name = claims.get('given_name', '')
@@ -58,7 +72,26 @@ class KeycloakOIDCAuthenticationBackend(OIDCAuthenticationBackend):
         user.email = email
         user.save()
         return user
-    
+
+    def get_username(self, claims):
+        """Extract username from claims"""
+        regex = re.compile(r"\s+")
+        if "username" in claims:
+            username = claims.get("username")
+        elif "preferred_username" in claims:
+            username = claims.get("preferred_username")
+        elif "name" in claims:
+            name = regex.sub('.', claims.get("name")).strip().lower()
+            username = generate_username(name)
+        elif "family_name" in claims and "given_name" in claims:
+            name = f'''{claims.get("given_name")} {claims.get("family_name")}'''
+            name = regex.sub('.', name).strip().lower()
+            username = generate_username(name)
+        else:
+            # Use the email address as a last resort
+            username = generate_username(self.get_email(claims))
+        return username
+
     def get_email(self, claims):
         """Extract email from claims"""
         email = ""
@@ -71,7 +104,6 @@ class KeycloakOIDCAuthenticationBackend(OIDCAuthenticationBackend):
             email = email[0]
         return email
 
-    
 
 def execute_logout(request):
     """
@@ -84,8 +116,9 @@ def execute_logout(request):
 
     return settings.LOGOUT_REDIRECT_URL
 
-def generate_username(email):
+
+def generate_username(identifier):
     # Using Python 3 and Django 1.11+, usernames can contain alphanumeric
     # (ascii and unicode), _, @, +, . and - characters. So we normalize
     # it and slice at 150 characters.
-    return unicodedata.normalize('NFKC', email)[:150]
+    return unicodedata.normalize('NFKC', identifier)[:150]
