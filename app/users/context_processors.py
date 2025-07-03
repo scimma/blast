43460e594fb5e75d83a6a57e2c4d36eb6c709198
T. Andrew Manning <manninga@illinois.edu>
2025-07-03 14:27:07 -0500

    Display retrigger and reprocess buttons based on user permissions

--- app/users/context_processors.py
+++ app/users/context_processors.py
@@ -9,6 +9,16 @@ def user_profile(request):
     def decoder(b64_string):
         return base64.urlsafe_b64decode(b64_string.encode('utf-8')).decode('utf-8')
 
+    def check_perms(user, perm):
+        if isinstance(perm, str):
+            perms = (perm,)
+        else:
+            perms = perm
+        # First check if the user has the permission (even anon users)
+        if user.has_perms(perms):
+            return True
+        return False
+
     user = auth(request)['user']
     username_b64decoded = ''
     for padded_string in [f'''{user.username}{pad}''' for pad in ['', '=', '==']]:
@@ -23,5 +33,11 @@ def user_profile(request):
             break
 
     logger.debug(f'''Decoded username: {username_b64decoded}''')
-    context = {'username_b64decoded': username_b64decoded}
+
+    context = {
+        'username_b64decoded': username_b64decoded,
+        'has_perm_retrigger_transient': check_perms(user, "host.retrigger_transient"),
+        'has_perm_reprocess_transient': check_perms(user, "host.reprocess_transient"),
+    }
+    logger.debug(context)
     return context
