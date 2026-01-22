2ae8bd094a8595673225d1ca2257112794cbba53
T. Andrew Manning <manninga@illinois.edu>
2026-01-22 14:36:36 -0600

    Code linting

--- app/entrypoints/initialize_data_dirs.sh
+++ app/entrypoints/initialize_data_dirs.sh
@@ -11,21 +11,6 @@ cd "${DATA_ROOT_DIR}"
 
 # The creation of symlinks should error if there is a non-symlink
 # file or folder where the symlink should be.
-# if [[ ! -L "${CUTOUT_ROOT}" ]]; then
-#   mkdir -p "${DATA_ROOT_DIR}"/cutout_cdn
-#   mkdir -p "$(dirname "${CUTOUT_ROOT}")"
-#   ln -s "${DATA_ROOT_DIR}/cutout_cdn" "${CUTOUT_ROOT}"
-# fi
-# if [[ ! -L "${CUTOUT_SCRATCH_ROOT}" ]]; then
-#   mkdir -p "${DATA_ROOT_DIR}"/cutout_scratch
-#   mkdir -p "$(dirname "${CUTOUT_SCRATCH_ROOT}")"
-#   ln -s "${DATA_ROOT_DIR}/cutout_scratch" "${CUTOUT_SCRATCH_ROOT}"
-# fi
-# if [[ ! -L "${SED_OUTPUT_ROOT}" ]]; then
-#   mkdir -p "${DATA_ROOT_DIR}"/sed_output
-#   mkdir -p "$(dirname "${SED_OUTPUT_ROOT}")"
-#   ln -s "${DATA_ROOT_DIR}/sed_output" "${SED_OUTPUT_ROOT}"
-# fi
 if [[ ! -L "${TNS_STAGING_ROOT}" ]]; then
   mkdir -p "${DATA_ROOT_DIR}"/tns_staging
   mkdir -p "$(dirname "${TNS_STAGING_ROOT}")"
