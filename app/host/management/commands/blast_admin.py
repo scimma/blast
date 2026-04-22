6d4f312d22d4fc4192c1d184f82c0f580e0077b0
T. Andrew Manning <manninga@illinois.edu>
2026-04-22 11:55:21 -0500

    Add util.py module for blast_admin management command

--- app/host/management/commands/blast_admin.py
+++ app/host/management/commands/blast_admin.py
@@ -1,6 +1,19 @@
+"""
+This module defines a custom Django management command "blast_admin". Its purpose is to
+aid Blast admin and development operations that require the Django environment, such as
+accessing database objects using the Django ORM.
+
+The usage syntax is as follows:
+
+   python manage.py blast_admin [func_name]
+
+where "func_name()" is defined in either "util.py" (public code) or "local_util.py"
+(local scratch ignored by Git).
+"""
 from django.core.management.base import BaseCommand
 from host.log import get_logger
 logger = get_logger(__name__)
+from .util import *  # noqa: F401,F403
 try:
     from .local_util import *  # noqa: F401,F403
     scratch_module_exists = True
