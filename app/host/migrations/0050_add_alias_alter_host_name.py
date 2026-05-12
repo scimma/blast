4193ea0cf68c9222ad390629c4aa00d87c521881
T. Andrew Manning <manninga@illinois.edu>
2026-05-12 11:43:15 -0500

    Require Host names to be unique

--- app/host/migrations/0050_add_alias_alter_host_name.py
+++ app/host/migrations/0050_add_alias_alter_host_name.py
@@ -12,6 +12,11 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.AlterField(
+            model_name='host',
+            name='name',
+            field=models.CharField(blank=True, max_length=100, null=True, unique=True),
+        ),
         migrations.CreateModel(
             name='Alias',
             fields=[
