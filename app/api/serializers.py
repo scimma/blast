8806aa572ec912681801b3fe84ade36a8ed3bd0b
Abh1ramManugur1 <abhiram.manuguri@gmail.com>
2026-07-17 11:54:07 -0500

    Removed Nested Serializers to avoid changing API output
    The only problem the nested serializers attempted to solve was the naming issue, which can be similarly solved by just reordering the serializer class definitions and calling them directly.
    
    Added extra StatusSerializer for TaskRegisterSerializer

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -2,106 +2,10 @@ from host import models
 from rest_framework import serializers
 from drf_spectacular.utils import extend_schema_field
 
-##################
-# Nested Serializers
-
-
-class UserNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.User
-        fields = ["id", "username"]
-
-
-class SurveyNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Survey
-        fields = ["id", "name"]
-
-
-class TaskNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Task
-        fields = ["id", "name"]
-
-
-class StatusNestedSerializer(serializers.ModelSerializer):
+class StatusSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Status
         fields = ["id", "message", "type"]
-
-
-class HostNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Host
-        fields = [
-            "id", "name", "ra_deg", "dec_deg",
-            "redshift", "redshift_err",
-            "photometric_redshift", "photometric_redshift_err",
-            "milkyway_dust_reddening", "catalog_name", "catalog_release",
-        ]
-
-
-# Depends on SurveyNestedSerializer
-class FilterNestedSerializer(serializers.ModelSerializer):
-    survey = SurveyNestedSerializer(read_only=True)
-
-    class Meta:
-        model = models.Filter
-        fields = [
-            "id", "name", "survey",
-            "wavelength_eff_angstrom", "wavelength_min_angstrom", "wavelength_max_angstrom",
-            "pixel_size_arcsec", "image_fwhm_arcsec",
-            "vega_zero_point_jansky", "magnitude_zero_point", "ab_offset",
-        ]
-
-
-class TransientNestedSerializer(serializers.ModelSerializer):
-    host = HostNestedSerializer(read_only=True)
-    added_by = UserNestedSerializer(read_only=True)
-
-    class Meta:
-        model = models.Transient
-        fields = [
-            "id", "name", "ra_deg", "dec_deg",
-            "tns_id", "tns_prefix", "public_timestamp",
-            "redshift", "spectroscopic_class", "photometric_class",
-            "milkyway_dust_reddening", "processing_status", "progress",
-            "host", "added_by",
-        ]
-
-
-class CutoutNestedSerializer(serializers.ModelSerializer):
-    filter = FilterNestedSerializer(read_only=True)
-    transient = TransientNestedSerializer(read_only=True)
-
-    class Meta:
-        model = models.Cutout
-        fields = ["id", "name", "filter", "transient", "message", "cropped"]
-
-
-# Depends on CutoutNestedSerializer and TransientNestedSerializer
-class ApertureNestedSerializer(serializers.ModelSerializer):
-    cutout = CutoutNestedSerializer(read_only=True)
-    transient = TransientNestedSerializer(read_only=True)
-
-    class Meta:
-        model = models.Aperture
-        fields = [
-            "id", "name", "ra_deg", "dec_deg",
-            "orientation_deg", "semi_major_axis_arcsec", "semi_minor_axis_arcsec",
-            "type", "cutout", "transient",
-        ]
-
-
-class StarFormationHistoryResultNestedSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.StarFormationHistoryResult
-        fields = [
-            "id",
-            "logsfr_16", "logsfr_50", "logsfr_84",
-            "logsfr_tmin", "logsfr_tmax",
-        ]
-
 
 class CutoutField(serializers.RelatedField):
     def to_representation(self, value):
@@ -109,9 +13,6 @@ class CutoutField(serializers.RelatedField):
 
 
 class TransientSerializer(serializers.ModelSerializer):
-
-    host = HostNestedSerializer(read_only=True)
-
     class Meta:
         model = models.Transient
         exclude = [
@@ -130,6 +31,26 @@ class TransientSerializer(serializers.ModelSerializer):
         aliases = models.Alias.objects.filter(transient=obj)
         return [alias.alias for alias in aliases]
 
+class FilterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Filter
+        depth = 1
+        fields = [
+            "name",
+            "pixel_size_arcsec",
+            "image_fwhm_arcsec",
+            "wavelength_eff_angstrom",
+            "ab_offset",
+        ]
+        
+class CutoutSerializer(serializers.ModelSerializer):
+    filter = FilterSerializer(read_only=True)
+    transient = TransientSerializer(read_only=True)
+
+    class Meta:
+        model = models.Cutout
+        depth = 1
+        exclude = ["fits"]
 
 class HostSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
@@ -156,8 +77,8 @@ class HostSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ApertureSerializer(serializers.ModelSerializer):
-    cutout = CutoutNestedSerializer(read_only=True)
-    transient = TransientNestedSerializer(read_only=True)
+    cutout = CutoutSerializer(read_only=True)
+    transient = TransientSerializer(read_only=True)
 
     class Meta:
         model = models.Aperture
@@ -166,9 +87,10 @@ class ApertureSerializer(serializers.ModelSerializer):
 
 
 class AperturePhotometrySerializer(serializers.ModelSerializer):
-    aperture = ApertureNestedSerializer(read_only=True)
-    filter = FilterNestedSerializer(read_only=True)
-    transient = TransientNestedSerializer(read_only=True)
+
+    aperture = ApertureSerializer(read_only=True)
+    filter = FilterSerializer(read_only=True)
+    transient = TransientSerializer(read_only=True)
 
     class Meta:
         model = models.AperturePhotometry
@@ -177,8 +99,8 @@ class AperturePhotometrySerializer(serializers.ModelSerializer):
 
 
 class AliasSerializer(serializers.ModelSerializer):
-    transient = TransientNestedSerializer(read_only=True)
-    host = HostNestedSerializer(read_only=True)
+    transient = TransientSerializer(read_only=True)
+    host = HostSerializer(read_only=True)
 
     class Meta:
         model = models.Alias
@@ -197,9 +119,8 @@ class AliasSerializer(serializers.ModelSerializer):
 
 
 class SEDFittingResultSerializer(serializers.ModelSerializer):
-    transient = TransientNestedSerializer(read_only=True)
-    aperture = ApertureNestedSerializer(read_only=True)
-    logsfh = StarFormationHistoryResultNestedSerializer(many=True, read_only=True)
+    transient = TransientSerializer(read_only=True)
+    aperture = ApertureSerializer(read_only=True)
 
     class Meta:
         model = models.SEDFittingResult
@@ -207,33 +128,16 @@ class SEDFittingResultSerializer(serializers.ModelSerializer):
         exclude = ["log_tau_16", "log_tau_50", "log_tau_84", "posterior"]
 
 
-class CutoutSerializer(serializers.ModelSerializer):
-    filter = FilterNestedSerializer(read_only=True)
-    transient = TransientNestedSerializer(read_only=True)
 
+class TaskSerializer(serializers.ModelSerializer):
     class Meta:
-        model = models.Cutout
-        depth = 1
-        exclude = ["fits"]
-
-
-class FilterSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Filter
-        depth = 1
-        fields = [
-            "name",
-            "pixel_size_arcsec",
-            "image_fwhm_arcsec",
-            "wavelength_eff_angstrom",
-            "ab_offset",
-        ]
-
+        model = models.Task
+        fields = ["name"]
 
 class TaskRegisterSerializer(serializers.ModelSerializer):
-    task = TaskNestedSerializer(read_only=True)
-    status = StatusNestedSerializer(read_only=True)
-    transient = TransientNestedSerializer(read_only=True)
+    task = TaskSerializer(read_only=True)
+    status = StatusSerializer(read_only=True)
+    transient = TransientSerializer(read_only=True)
 
     class Meta:
         model = models.TaskRegister
@@ -241,7 +145,3 @@ class TaskRegisterSerializer(serializers.ModelSerializer):
         fields = "__all__"
 
 
-class TaskSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Task
-        fields = ["name"]
