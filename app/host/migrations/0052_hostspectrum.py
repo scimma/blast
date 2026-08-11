0dd47939b6778f389b2e9979b3ed04b7206278c7
T. Andrew Manning <manninga@illinois.edu>
2026-08-11 17:06:21 -0500

    Consolidate migration scripts

--- app/host/migrations/0052_hostspectrum.py
+++ app/host/migrations/0052_hostspectrum.py
@@ -4,6 +4,57 @@ import django.db.models.deletion
 import host.models
 from django.db import migrations, models
 
+HOST_SPECTRUM_DOWNLOAD_TASK_NAME = 'Host spectrum download'
+HOST_SPECTRUM_THUMBNAIL_TASK_NAME = 'Generate thumbnail host spectrum'
+
+
+def add_host_spectrum_task(apps, schema_editor):
+    '''Add the new Host spectrum download task definition'''
+    Task = apps.get_model("host", "Task")
+    Status = apps.get_model("host", "Status")
+    Task.objects.create(name=HOST_SPECTRUM_DOWNLOAD_TASK_NAME)
+    Status.objects.create(message='no host spectrum', type='error')
+
+
+def add_host_spectrum_taskregisters(apps, schema_editor, new_task_name=''):
+    add_spectrum_taskregisters(apps, schema_editor, new_task_name=HOST_SPECTRUM_DOWNLOAD_TASK_NAME)
+
+
+def add_thumbnail_spectrum_task(apps, schema_editor):
+    '''Add the new Generate thumbnail host spectrum task definition'''
+    Task = apps.get_model("host", "Task")
+    Task.objects.create(name=HOST_SPECTRUM_THUMBNAIL_TASK_NAME)
+
+
+def add_thumbnail_spectrum_taskregisters(apps, schema_editor, new_task_name=''):
+    add_spectrum_taskregisters(apps, schema_editor, new_task_name=HOST_SPECTRUM_THUMBNAIL_TASK_NAME)
+
+
+def add_spectrum_taskregisters(apps, schema_editor, new_task_name=''):
+    '''Add task registers for the new task to existing transients so that they will run when retriggered.'''
+    Transient = apps.get_model("host", "Transient")
+    TaskRegister = apps.get_model("host", "TaskRegister")
+    Task = apps.get_model("host", "Task")
+    Status = apps.get_model("host", "Status")
+    not_processed_status = Status.objects.get(message__exact="not processed")
+    task = Task.objects.get(name=new_task_name)
+
+    all_transients = Transient.objects.all()
+    all_trs = TaskRegister.objects.select_related('transient').select_related('task').all()
+    num_transients = len(all_transients)
+    for idx, transient in enumerate(all_transients):
+        print(f'[{idx + 1}/{num_transients}] Adding "{HOST_SPECTRUM_THUMBNAIL_TASK_NAME}" '
+              f'task register for transient "{transient.name}"...')
+        if not all_trs.filter(transient=transient, task=task):
+            TaskRegister.objects.create(
+                transient=transient,
+                task=task,
+                status=not_processed_status,
+            )
+        else:
+            print(f'''WARNING: Unexpected TaskRegister object already exists for "{new_task_name}" '''
+                  f'''on transient "{transient.name}".''')
+
 
 class Migration(migrations.Migration):
 
@@ -30,4 +81,8 @@ class Migration(migrations.Migration):
                 ('transient', models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE, to='host.transient')),  # noqa
             ],
         ),
+        migrations.RunPython(add_host_spectrum_task),
+        migrations.RunPython(add_thumbnail_spectrum_task),
+        migrations.RunPython(add_host_spectrum_taskregisters),
+        migrations.RunPython(add_thumbnail_spectrum_taskregisters),
     ]
