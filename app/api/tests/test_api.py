f67d75efd9bd553c9cee47ce68edd7d8feb30528
T. Andrew Manning <manninga@illinois.edu>
2026-08-12 14:27:36 -0500

    Update /api/dataset unit tests and add delete test

--- app/api/tests/test_api.py
+++ app/api/tests/test_api.py
@@ -8,6 +8,10 @@ from rest_framework.test import APIClient
 from rest_framework import status
 from django.contrib.contenttypes.models import ContentType
 from host.models import Alias
+from host.models import Transient
+from host.host_utils import import_transient_info
+from host.object_store import ObjectStore
+from django.conf import settings
 
 
 class APITest(TestCase):
@@ -15,27 +19,70 @@ class APITest(TestCase):
 
     def setUp(self):
         self.client = Client()
+        with open('''/data/transient_datasets/2026dix.tar.gz''', 'rb') as dataset_fileobj:
+            import_transient_info(dataset_fileobj)
 
-    def test_transient_get(self):
+    def test_dataset_get(self):
         client = APIClient()
         # Load expected data
         with open(os.path.join(Path(__file__).resolve().parent, '2022testone_get_test.json')) as fp:
             expected_data = json.load(fp)
         expected_data.pop('metadata')
         # Fetch data from API
-        request = client.get("/api/transient/get/2022testone/")
+        request = client.get("/api/dataset/2022testone/")
         data = json.loads(request.content)
         # Remove the metadata content that contains the generation timestamp
         data.pop('metadata')
         self.assertTrue(data == expected_data)
         self.assertTrue(request.status_code == status.HTTP_200_OK)
 
-    def test_get_no_transient(self):
+    def test_dataset_get_missing(self):
         client = APIClient()
-        request = client.get("/api/transient/get/2022NotInDatabase/")
+        request = client.get("/api/dataset/2022NotInDatabase/")
         self.assertTrue(request.status_code == status.HTTP_404_NOT_FOUND)
         data = json.loads(request.content)
         self.assertTrue(data["message"] == "2022NotInDatabase not in database")
+
+    def test_dataset_delete(self):
+        # Create a temporary user and authenticate them
+        user = User.objects.create_user(username="testola", password='password')
+        self.client.force_login(user)
+        transient_name = '2022testone'
+        # Attempt to delete the dataset without permission
+        response = self.client.delete(f'/api/dataset/{transient_name}/')
+        self.assertTrue(response.status_code == status.HTTP_403_FORBIDDEN)
+        # Grant the user permission
+        add_permission = Permission.objects.get(
+            codename="delete_transient",
+            content_type=ContentType.objects.get_for_model(Transient),
+        )
+        user.user_permissions.add(add_permission)
+        assert user.has_perm('host.delete_transient')
+        # Try again with permission
+        response = self.client.delete(f'/api/dataset/{transient_name}/')
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+
+    def test_dataset_delete_with_files(self):
+        # Create a temporary user and authenticate them
+        user = User.objects.create_user(username="testola", password='password')
+        self.client.force_login(user)
+        transient_name = '2026dix'
+        # Attempt to delete the dataset without permission
+        response = self.client.delete(f'/api/dataset/{transient_name}/?files=true')
+        self.assertTrue(response.status_code == status.HTTP_403_FORBIDDEN)
+        # Grant the user permission
+        add_permission = Permission.objects.get(
+            codename="delete_transient",
+            content_type=ContentType.objects.get_for_model(Transient),
+        )
+        user.user_permissions.add(add_permission)
+        assert user.has_perm('host.delete_transient')
+        # Try again with permission
+        response = self.client.delete(f'/api/dataset/{transient_name}/?files=true')
+        self.assertTrue(response.status_code == status.HTTP_204_NO_CONTENT)
+        s3 = ObjectStore()
+        self.assertFalse(s3.object_exists(os.path.join(settings.S3_BASE_PATH, settings.CUTOUT_ROOT.strip('/'),
+                                                       '2026dix/PanSTARRS/PanSTARRS_g.jpg')))
 
     def test_alias(self):
         # Create a temporary user and authenticate them
